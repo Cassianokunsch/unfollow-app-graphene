d441fe80d0a5217535978c0cc0a52443b26a17b0
Cassiano Kunsch das Neves <cassiano.neves@avista.local>
2020-02-06 18:50:43 -0300

    create constants

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+INVALID_CREDENTIALS = "Usuário ou senha inválidos!"
+UNAUTHORIZED = "Não autorizado. Não há sessão para esse usuário!"
+TOKEN_ERROR = 'Essa requisição precisa do token JWT'
+UNKNOW_ERROR = "Erro desconhecido"
+SECRET = 'secret'
+LOGOUT_ERROR = "Ocorreu um erro ao tentar deslogar!"
+UNFOLLOW_ERROR = "Ocorreu um problema na hora de parar de seguir o usuário!"
+FOLLOW_ERROR = "Ocorreu um problema na hora de seguir o usuário!"
