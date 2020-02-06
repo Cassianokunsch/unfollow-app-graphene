c3f69bcb3f1d8674d9c89881536f585576be6c44
Cassiano Kunsch das Neves <cassiano.neves@avista.local>
2020-02-06 16:30:16 -0300

    rename resolvers and set not nullable

--- src/queries.py
+++ src/queries.py
@@ -1,23 +1,26 @@
-from graphene import ObjectType, List, Field
-from api.instagram import get_total_followers, get_not_followers, get_total_followings
+from graphene import ObjectType, List, Field, NonNull, String
+from instagramApi import get_total_followers, get_not_followers, get_total_followings
 from utils import get_current_user
 from typess import FollowerType, UnfollowerType, FollowingType, UserType
 
 
 class Query(ObjectType):
-    me = Field(UserType)
-    my_followers = List(FollowerType)
-    my_following = List(FollowingType)
-    my_unfollowers = List(UnfollowerType)
+    me = String()
+    list_my_followers = List(NonNull(FollowerType))
+    list_my_followings = List(NonNull(FollowingType))
+    list_my_unfollowers = List(NonNull(UnfollowerType))
 
-    def resolve_my_followers(self, info):
+    def resolve_me(self, info):
+        return "teste"
+
+    def resolve_list_my_followers(self, info):
         user = get_current_user(info.context)
         return get_total_followers(user['id'])
 
-    def resolve_my_following(self, info):
+    def resolve_list_my_followings(self, info):
         user = get_current_user(info.context)
         return get_total_followings(user['id'])
 
-    def resolve_my_unfollowers(self, info):
+    def resolve_list_my_unfollowers(self, info):
         user = get_current_user(info.context)
         return get_not_followers(user['id'])
