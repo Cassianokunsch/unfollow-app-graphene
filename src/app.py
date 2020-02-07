ca4037741465dcacffb6ddf3cf1b31de55db08cd
Cassiano Kunsch das Neves <cassiano.neves@avista.local>
2020-02-07 18:11:20 -0300

    Changing somethings

--- src/app.py
+++ src/app.py
@@ -1,18 +1,19 @@
 from flask import Flask
 from flask_graphql import GraphQLView
 from schema import schema
+import os
 
 
 app = Flask(__name__)
-app.debug = True
 
 app.add_url_rule(
     '/',
     view_func=GraphQLView.as_view(
         'graphql',
-        schema=schema
+        schema=schema,
+        graphiql=True
     )
 )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', "5000"))
