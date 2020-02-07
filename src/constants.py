8c710e927a6a0138de2c3633fc733aa2dc60ae2d
Cassiano Künsch das Neves <cassiano.kunsch@outlook.com>
2020-02-07 19:02:22 -0300

    Update constants.py
    Get secret from environment

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,10 @@
+import os
+
 INVALID_CREDENTIALS = "Usuário ou senha inválidos!"
 UNAUTHORIZED = "Não autorizado. Não há sessão para esse usuário!"
 TOKEN_ERROR = 'Essa requisição precisa do token JWT'
 UNKNOW_ERROR = "Erro desconhecido"
-SECRET = 'secret'
+SECRET = os.environ.get('SECRET', "secret")
 LOGOUT_ERROR = "Ocorreu um erro ao tentar deslogar!"
 UNFOLLOW_ERROR = "Ocorreu um problema na hora de parar de seguir o usuário!"
 FOLLOW_ERROR = "Ocorreu um problema na hora de seguir o usuário!"
